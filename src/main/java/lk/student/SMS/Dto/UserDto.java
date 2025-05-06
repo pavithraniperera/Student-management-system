0e8bc82e61a0f0f4101695e30af8b0847c4c09f2
pavithrani <pererapavithrani02@gmail.com>
2025-05-06 11:53:02 +0530

    validated and apply getter setter

--- src/main/java/lk/student/SMS/Dto/UserDto.java
+++ src/main/java/lk/student/SMS/Dto/UserDto.java
@@ -3,25 +3,98 @@ package lk.student.SMS.Dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lk.student.SMS.Entity.Role;
 import lombok.Data;
+import lombok.ToString;
 
-@Data
+
 public class UserDto {
+
+
     @NotBlank(message = "Name is required")
     private String name;
 
-    @Email(message = "Invalid email format")
     @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
 
     @NotBlank(message = "Phone is required")
+    @Size(min = 10, max = 15, message = "Phone number should be between 10 and 15 characters")
     private String phone;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password should be at least 6 characters")
     private String password;
 
     @NotNull(message = "Role is required")
     private Role role; // Enum: STUDENT, COUNSELOR, FINANCE_MANAGER
+
+    public UserDto() {
+    }
+
+    public UserDto(String name, String email, String phone, String password, Role role) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
 }
+
 
--- src/main/java/lk/student/SMS/Entity/User.java
+++ src/main/java/lk/student/SMS/Entity/User.java
@@ -1,12 +1,12 @@
 package lk.student.SMS.Entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 import java.util.List;
 
 
-@Data
+
 
 @Entity
 @Table(name = "users")
@@ -22,6 +22,84 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, String password, Role role, List<Student> registeredStudents, Student studentProfile) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.role = role;
+        this.registeredStudents = registeredStudents;
+        this.studentProfile = studentProfile;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Student> getRegisteredStudents() {
+        return registeredStudents;
+    }
+
+    public void setRegisteredStudents(List<Student> registeredStudents) {
+        this.registeredStudents = registeredStudents;
+    }
+
+    public Student getStudentProfile() {
+        return studentProfile;
+    }
+
+    public void setStudentProfile(Student studentProfile) {
+        this.studentProfile = studentProfile;
+    }
 
     @OneToMany(mappedBy = "registeredBy")
     private List<Student> registeredStudents;
