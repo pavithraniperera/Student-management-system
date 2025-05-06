43fab4d29f31731580110446594f78078a3b25dc
pavithrani <pererapavithrani02@gmail.com>
2025-05-06 09:25:31 +0530

    create UserRepository

---
+++ src/main/java/lk/student/SMS/Dao/UserRepository.java
@@ -0,0 +1,9 @@
+package lk.student.SMS.Dao;
+
+import lk.student.SMS.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);  // Custom query method
+}
