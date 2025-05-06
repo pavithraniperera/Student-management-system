7b0895988d1311394e8c90e32bb3e9b1066e5902
pavithrani <pererapavithrani02@gmail.com>
2025-05-06 09:34:29 +0530

    create IntakeRepository

---
+++ src/main/java/lk/student/SMS/Dao/IntakeRepository.java
@@ -0,0 +1,8 @@
+package lk.student.SMS.Dao;
+
+import lk.student.SMS.Entity.Intake;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IntakeRepository extends JpaRepository<Intake, Long> {
+
+}
