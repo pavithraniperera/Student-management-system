829949441a992e651f789467e1a496bde30c7fd3
pavithrani <pererapavithrani02@gmail.com>
2025-05-06 10:21:14 +0530

    added validation to user

--- src/main/java/lk/student/SMS/Dto/UserDto.java
+++ src/main/java/lk/student/SMS/Dto/UserDto.java
@@ -1,15 +1,27 @@
 package lk.student.SMS.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lk.student.SMS.Entity.Role;
 import lombok.Data;
 
 @Data
 public class UserDto {
-    private Long id;
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Phone is required")
     private String phone;
+
+    @NotBlank(message = "Password is required")
     private String password;
-    private Role role;
+
+    @NotNull(message = "Role is required")
+    private Role role; // Enum: STUDENT, COUNSELOR, FINANCE_MANAGER
 }
 
