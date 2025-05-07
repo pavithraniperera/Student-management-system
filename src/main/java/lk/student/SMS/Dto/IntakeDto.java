c7186c42e9f303df73a8e0607018b8f3245b79b9
pavithrani <pererapavithrani02@gmail.com>
2025-05-07 09:18:16 +0530

    Added getter and setter

--- src/main/java/lk/student/SMS/Dto/IntakeDto.java
+++ src/main/java/lk/student/SMS/Dto/IntakeDto.java
@@ -1,13 +1,65 @@
 package lk.student.SMS.Dto;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
 
-@Data
+
 public class IntakeDto {
     private Long id;
+
+    @NotBlank(message = "Intake name is required")
     private String name;
+
+    @NotNull(message = "Start date is required")
+    @FutureOrPresent(message = "Start date cannot be in the past")
     private LocalDate startDate;
+
+    @NotNull(message = "End date is required")
     private LocalDate endDate;
+
+    public IntakeDto() {
+    }
+
+    public IntakeDto(Long id, String name, LocalDate startDate, LocalDate endDate) {
+        this.id = id;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
--- src/main/java/lk/student/SMS/Entity/Intake.java
+++ src/main/java/lk/student/SMS/Entity/Intake.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "intakes")
-@Data
+
 public class Intake {
 
     @Id
@@ -19,6 +19,9 @@ public class Intake {
     private Long intakeId;
 
     private String name;
+
+
+
     private LocalDate startDate;
     private LocalDate endDate;
 
@@ -30,5 +33,47 @@ public class Intake {
     public void addBatch(Batch batch) {
         batches.add(batch);
         batch.setIntake(this);
+    }
+    public Intake() {
+    }
+
+    public Long getIntakeId() {
+        return intakeId;
+    }
+
+    public void setIntakeId(Long intakeId) {
+        this.intakeId = intakeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<Batch> getBatches() {
+        return batches;
+    }
+
+    public void setBatches(List<Batch> batches) {
+        this.batches = batches;
     }
 }
