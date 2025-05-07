1409cdb261497b6aba52c8b55710911564814e4f
pavithrani <pererapavithrani02@gmail.com>
2025-05-07 15:52:35 +0530

    add configuration beans

--- src/main/java/lk/student/SMS/SmsApplication.java
+++ src/main/java/lk/student/SMS/SmsApplication.java
@@ -4,10 +4,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
+//@EnableWebSecurity
+//@EnableMethodSecurity
 public class SmsApplication {
 
 	public static void main(String[] args) {
@@ -17,5 +25,21 @@ public class SmsApplication {
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(passwordEncoder);
+		return provider;
+	}
+
+
+
+
 
 }
