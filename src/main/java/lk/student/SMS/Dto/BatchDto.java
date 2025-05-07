a2773b41984919ec5dbccc600c0af53354f79f04
pavithrani <pererapavithrani02@gmail.com>
2025-05-07 10:13:05 +0530

    add getter and setters

--- src/main/java/lk/student/SMS/Dto/BatchDto.java
+++ src/main/java/lk/student/SMS/Dto/BatchDto.java
@@ -1,12 +1,60 @@
 package lk.student.SMS.Dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-@Data
+
 public class BatchDto {
     private Long id;
+    @NotBlank(message = "Batch name is required")
     private String batchName;
 
+    @NotNull(message = "Intake ID is required")
     private Long intakeId;
+
+    @NotNull(message = "Course ID is required")
     private Long courseId;
+
+    public BatchDto() {
+    }
+
+    public BatchDto(Long id, String batchName, Long intakeId, Long courseId) {
+        this.id = id;
+        this.batchName = batchName;
+        this.intakeId = intakeId;
+        this.courseId = courseId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBatchName() {
+        return batchName;
+    }
+
+    public void setBatchName(String batchName) {
+        this.batchName = batchName;
+    }
+
+    public Long getIntakeId() {
+        return intakeId;
+    }
+
+    public void setIntakeId(Long intakeId) {
+        this.intakeId = intakeId;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
 }
--- src/main/java/lk/student/SMS/Entity/Batch.java
+++ src/main/java/lk/student/SMS/Entity/Batch.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "batches")
-@Data
+
 public class Batch {
 
     @Id
@@ -18,6 +18,7 @@ public class Batch {
     private Long batchId;
 
     private String batchName;
+
 
 
     // One batch belongs to one intake (Many-to-One)
@@ -39,5 +40,39 @@ public class Batch {
 
     public void setCourse(Course course) {
         this.course = course;
+    }
+    public Batch() {
+    }
+
+    public Long getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(Long batchId) {
+        this.batchId = batchId;
+    }
+
+    public String getBatchName() {
+        return batchName;
+    }
+
+    public void setBatchName(String batchName) {
+        this.batchName = batchName;
+    }
+
+    public Intake getIntake() {
+        return intake;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
     }
 }
