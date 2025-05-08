a25718e88db027f96e1bf1ed388460f3db61d6ae
pavithrani <pererapavithrani02@gmail.com>
2025-05-08 13:27:16 +0530

    add Role based management to user

--- src/main/java/lk/student/SMS/Controller/UserController.java
+++ src/main/java/lk/student/SMS/Controller/UserController.java
@@ -6,6 +6,7 @@ import lk.student.SMS.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,27 +21,32 @@ public class UserController {
         this.userService = userService;
     }
     // Create a new user
+    @PreAuthorize("hasAnyRole('COUNSELOR', 'FINANCE_MANAGER')")
     @PostMapping( consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userCreateDTO) {
         System.out.println(userCreateDTO);
         UserDto createdUser = userService.createUser(userCreateDTO);
         return ResponseEntity.ok(createdUser);
     }
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
-
+    @PreAuthorize("hasAnyRole('COUNSELOR', 'FINANCE_MANAGER')")
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
+
+    @PreAuthorize("hasRole('FINANCE_MANAGER')")
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
+    @PreAuthorize("hasRole('COUNSELOR')")
     @PutMapping("/{id}")
     public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @Valid @RequestBody UserDto userDto) {
         UserDto updatedUser = userService.updateUser(id, userDto);
