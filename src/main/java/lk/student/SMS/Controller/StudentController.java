8707ba418b491afc1b0d5a59e0acd76bc72cf22e
pavithrani <pererapavithrani02@gmail.com>
2025-05-08 13:27:09 +0530

    add Role based management to student

--- src/main/java/lk/student/SMS/Controller/StudentController.java
+++ src/main/java/lk/student/SMS/Controller/StudentController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lk.student.SMS.Dto.StudentDto;
 import lk.student.SMS.Service.StudentService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,26 +17,30 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-
+    @PreAuthorize("hasAnyRole('COUNSELOR')")
     @PostMapping
     public ResponseEntity<StudentDto> createStudent(@Valid @RequestBody StudentDto studentDto) {
         return ResponseEntity.ok(studentService.createStudent(studentDto));
     }
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}")
     public ResponseEntity<StudentDto> getStudent(@PathVariable Long id) {
         return ResponseEntity.ok(studentService.getStudentById(id));
     }
 
+    @PreAuthorize("hasAnyRole('COUNSELOR', 'FINANCE_MANAGER')")
     @GetMapping
     public ResponseEntity<List<StudentDto>> getAllStudents() {
         return ResponseEntity.ok(studentService.getAllStudents());
     }
 
+    @PreAuthorize("hasRole('COUNSELOR')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
         studentService.deleteStudent(id);
         return ResponseEntity.noContent().build();
     }
+    @PreAuthorize("hasRole('COUNSELOR')")
     @PutMapping("/{id}")
     public ResponseEntity<StudentDto> updateStudent(@PathVariable Long id,
                                                     @Valid @RequestBody StudentDto studentDto) {
