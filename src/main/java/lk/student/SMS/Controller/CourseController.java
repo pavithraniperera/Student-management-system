4c8d7ebd23c62298d0174d60681017dcd4b93391
pavithrani <pererapavithrani02@gmail.com>
2025-05-08 13:26:49 +0530

    add Role based management to Course

--- src/main/java/lk/student/SMS/Controller/CourseController.java
+++ src/main/java/lk/student/SMS/Controller/CourseController.java
@@ -5,6 +5,7 @@ import lk.student.SMS.Dto.CourseDto;
 import lk.student.SMS.Service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,26 +21,31 @@ public class CourseController {
         this.courseService = courseService;
     }
 
+    @PreAuthorize("hasAnyRole('COUNSELOR', 'FINANCE_MANAGER')")
     @PostMapping
     public ResponseEntity<CourseDto> createCourse(@Valid @RequestBody CourseDto courseDto) {
         System.out.println(courseDto);
         return ResponseEntity.ok(courseService.createCourse(courseDto));
     }
+    @PreAuthorize("hasRole('COUNSELOR')")
     @PutMapping("/{id}")
     public ResponseEntity<CourseDto> updateCourse(@PathVariable Long id, @Valid @RequestBody CourseDto courseDto) {
         return ResponseEntity.ok(courseService.updateCourse(id, courseDto));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}")
     public ResponseEntity<CourseDto> getCourseById(@PathVariable Long id) {
         return ResponseEntity.ok(courseService.getCourseById(id));
     }
 
+    @PreAuthorize("hasAnyRole('STUDENT', 'COUNSELOR', 'FINANCE_MANAGER')")
     @GetMapping
     public ResponseEntity<List<CourseDto>> getAllCourses() {
         return ResponseEntity.ok(courseService.getAllCourses());
     }
 
+    @PreAuthorize("hasRole('COUNSELOR')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
         courseService.deleteCourse(id);
