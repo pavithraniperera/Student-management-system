6f38277957f151f2ee43b9fe964b713245203757
pavithrani <pererapavithrani02@gmail.com>
2025-05-09 10:32:42 +0530

    Add Repository annotation

--- src/main/java/lk/student/SMS/Dao/FeeSchemeRepository.java
+++ src/main/java/lk/student/SMS/Dao/FeeSchemeRepository.java
@@ -2,6 +2,8 @@ package lk.student.SMS.Dao;
 
 import lk.student.SMS.Entity.FeeScheme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FeeSchemeRepository extends JpaRepository<FeeScheme, Long> {}
 
--- src/main/java/lk/student/SMS/Dao/PaymentPlanRepository.java
+++ src/main/java/lk/student/SMS/Dao/PaymentPlanRepository.java
@@ -2,9 +2,11 @@ package lk.student.SMS.Dao;
 
 import lk.student.SMS.Entity.PaymentPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PaymentPlanRepository extends JpaRepository<PaymentPlan, Long> {
     Optional<PaymentPlan> findByFeeTypeAndAmountAndDescription(String feeType, Double amount, String description);
 
